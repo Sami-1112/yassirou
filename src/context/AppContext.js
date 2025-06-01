1a31d38cbb6c88a74ddde4059f224093de223174
Sami <samielkomy123@gmail.com>
2025-06-01 20:58:10 +0300

    Fix: Remove unused useEffect import in AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,5 +1,5 @@
 // src/context/AppContext.js
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import ar from '../locales/ar';
 import en from '../locales/en';
 
